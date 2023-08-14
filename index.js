97e441d1f6db952deffe48b1ccf99c6c59c92d6a
thongtr-dev <thongtr.dev@gmail.com>
2023-08-15 00:25:44 +0700

    Part 3 notes backend: part 3-2

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log("Method: ", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(requestLogger);
+
 let notes = [
   {
     id: 1,
@@ -64,8 +77,6 @@ app.get("/api/notes/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
-
-  response.json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -74,6 +85,8 @@ app.delete("/api/notes/:id", (request, response) => {
 
   response.status(204).end();
 });
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
